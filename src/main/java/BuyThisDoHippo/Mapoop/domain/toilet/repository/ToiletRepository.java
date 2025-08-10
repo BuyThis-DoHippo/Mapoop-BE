b8426d9c74651d6c048192ed028ba21479341315
jjmmnd <rnastery1@gmail.com>
2025-08-10 23:01:21 +0900

    [fix] distance 응답 단위 km -> m 변경

--- src/main/java/BuyThisDoHippo/Mapoop/domain/toilet/repository/ToiletRepository.java
+++ src/main/java/BuyThisDoHippo/Mapoop/domain/toilet/repository/ToiletRepository.java
@@ -19,26 +19,27 @@ public interface ToiletRepository extends JpaRepository<Toilet, Long> {
     List<Toilet> findAllByOrderByAvgRatingDesc(Pageable pageable);
 
     @Query(value = """
-
-            SELECT 
-               t.id as id,
-               t.name as name,
-               t.type as type,
-               t.is_partnership as isPartnership,
-               t.latitude as latitude,
-               t.longitude as longitude,
-               t.address as address,
-               t.floor as floor,
-               t.avg_rating as avgRating,
-               t.total_reviews as totalReviews,
-               (6371 * acos(cos(radians(:userLat)) * cos(radians(t.latitude)) 
-                          * cos(radians(t.longitude) - radians(:userLng)) 
-                          + sin(radians(:userLat)) * sin(radians(t.latitude)))) AS distance
-        FROM toilet t
-        HAVING distance <= :radiusKm 
-        ORDER BY distance
-        LIMIT :limit
-        """, nativeQuery = true)
+    SELECT 
+       t.id as id,
+       t.name as name,
+       t.type as type,
+       t.is_partnership as isPartnership,
+       t.latitude as latitude,
+       t.longitude as longitude,
+       t.address as address,
+       t.floor as floor,
+       t.avg_rating as avgRating,
+       t.total_reviews as totalReviews,
+       (6371 * acos(
+            cos(radians(:userLat)) * cos(radians(t.latitude)) 
+          * cos(radians(t.longitude) - radians(:userLng)) 
+          + sin(radians(:userLat)) * sin(radians(t.latitude))
+       ) * 1000) AS distance
+    FROM toilet t
+    HAVING (distance / 1000) <= :radiusKm
+    ORDER BY distance
+    LIMIT :limit
+    """, nativeQuery = true)
     List<ToiletWithDistance> findNearbyToilets(@Param("userLat") double userLatitude,
                                                @Param("userLng") double userLongitude,
                                                @Param("radiusKm") double radiusKm,
